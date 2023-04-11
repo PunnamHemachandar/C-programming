ad03d252a797d6f55d7b2b9c58a51400f482aa36
PunnamHemachandar <129481466+PunnamHemachandar@users.noreply.github.com>
2023-04-11 19:10:29 +0530

    Create sharing_string_literals.c

---
+++ Strings/sharing_string_literals.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  char *str1="hello";
+  char *str2="hello";
+  if(str1==str2)
+  {
+  printf("Ho they share same space");
+  }
+  else
+  {
+  printf("they are in different space");
+  }
+  
