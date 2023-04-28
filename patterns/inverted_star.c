761c5f7b27edbf3f23c162297bf9cea852a7feb3
PunnamHemachandar <129481466+PunnamHemachandar@users.noreply.github.com>
2023-04-28 16:29:58 +0530

    Create inverted_star.c

---
+++ patterns/inverted_star.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,rows,cols;
+    printf("enter the rows");
+    scanf("%d",&rows);
+    printf("enter the cols");
+    scanf("%d",&cols);
+    for(i=rows; i>=1; i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+/*
+output:
+rows:5
+cols:5
+*****
+****
+***
+**
+*
+
+*/
